63a2538c801100e22337d2b66d6c8740b1a18e50
ChuChaoliang <3105775144@qq.com>
2019-09-02 12:07:47 +0800

    '第一次提交'

---
+++ src/main/java/com/ccl/blog/CclBlogApplication.java
@@ -0,0 +1,13 @@
+package com.ccl.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CclBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CclBlogApplication.class, args);
+    }
+
+}
